57cfd45fc8da3ada3dd6a1f605f566520b0877aa
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2021-07-30 02:19:12 -0400

    seeds

--- data/seeds/003_ingredients.js
+++ data/seeds/003_ingredients.js
@@ -4,7 +4,7 @@ exports.seed = function(knex) {
         {ingredient_name: 'olive oil', recipe_id: 1, step_id: 2},
         {ingredient_name: 'spaghetti', recipe_id: 1, step_id: 2},
         {ingredient_name: 'tomatoes', recipe_id: 1, step_id: 2},
-        {ingredient_name: 'eggs', recipe_id: 2, step_id: null},
-        {ingredient_name: 'butter', recipe_id: 2, step_id: null},
+        {ingredient_name: 'eggs', recipe_id: 2, step_id: 3},
+        {ingredient_name: 'butter', recipe_id: 2, step_id: 5},
       ]);
     };
