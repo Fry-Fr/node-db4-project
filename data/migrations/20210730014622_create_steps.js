9004c182a1bcd6c3ace129754106651918aec2b7
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2021-07-29 22:15:39 -0400

    migrations

---
+++ data/migrations/20210730014622_create_steps.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('steps', tbl => {
+      tbl.increments('step_id');
+      tbl.integer('step_number')
+        .notNullable();
+      tbl.string('step_instuctions')
+        .notNullable();
+      tbl.integer('recipe_id')
+        .unsigned()
+        .references('recipes.recipe_id')
+        .notNullable()
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.droptableIfExists('steps');
+};
